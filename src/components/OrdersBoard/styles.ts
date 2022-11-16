da363be0a9bddc82aa38b48fdca4ae23d55d0ae4
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-16 14:47:14 -0300

    feat(OrderBoard.tsx): create component

---
+++ src/components/OrdersBoard/styles.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const Board = styled.div`
+  padding: 1rem;
+  border: 1px solid rgba(204, 204, 204, 0.4);
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  header {
+    padding: 0.5rem;
+    font-size: 0.875rem;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+`;
+
+export const OrdersContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-top: 1.5rem;
+
+  button {
+    width: 100%;
+    background-color: #fff;
+    border: 1px solid rgba(204, 204, 204, 0.4);
+    height: 128px;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0.25rem;
+
+    strong {
+      font-weight: 500;
+    }
+
+    span {
+      font-size: 0.875rem;
+      color: #666;
+    }
+
+    & + button {
+      margin-top: 1.5rem;
+    }
+  }
+`;
