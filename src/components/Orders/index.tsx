c2c02316ae5f2efc0ee8906eb9afeb7798f96b9e
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-19 11:35:51 -0300

    feat: filter orders

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from 'react';
 import { OrdersBoard } from '../OrdersBoard';
 import { Container } from './styles';
-
-import { orders } from '../../mocks/Orders.mock';
+import { Order } from '../../types/Order';
+import { api } from '../../libs/axios';
 
 export function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect(() => {
+    api.get('/orders').then((response) => setOrders(response.data));
+  }, []);
+
+  const waiting = orders.filter((order) => order.status === 'WAITING');
+  const production = orders.filter((order) => order.status === 'IN_PRODUCTION');
+  const done = orders.filter((order) => order.status === 'DONE');
+
   return (
     <Container>
-      <OrdersBoard icon="🕒" title="Fila de espera" orders={orders} />
-      <OrdersBoard icon="👨‍🍳" title="Em produção" orders={[]} />
-      <OrdersBoard icon="✅" title="Pronto" orders={[]} />
+      <OrdersBoard icon="🕒" title="Fila de espera" orders={waiting} />
+      <OrdersBoard icon="👨‍🍳" title="Em produção" orders={production} />
+      <OrdersBoard icon="✅" title="Pronto" orders={done} />
     </Container>
   );
 }
--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://6915-168-195-96-104.sa.ngrok.io',
+  baseURL: ' http://localhost:5000',
 });
